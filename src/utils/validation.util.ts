48d45e1837e50e871aecefc7604490af49a7492c
Esmail Elmoussel <elmoussel12@gmail.com>
2023-11-19 22:31:22 +0200

    feat: add password validation util

---
+++ src/utils/validation.util.ts
@@ -0,0 +1,24 @@
+/**
+ * Password validation function
+ * @param password
+ * @returns an error string or null if no error found!
+ */
+function validatePassword(password: string): string | null {
+  const hasLetter = /[a-zA-Z]/.test(password);
+  const hasNumber = /\d/.test(password);
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+  if (!hasLetter) {
+    return "Password must contain at least 1 letter";
+  }
+
+  if (!hasNumber) {
+    return "Password must contain at least 1 number";
+  }
+
+  if (!hasSpecialChar) {
+    return "Password must contain at least 1 special character";
+  }
+
+  return null;
+}
